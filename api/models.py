e196c9fe30e0a5aaccc2337043b07ca0101adfff
sohaila-elbasel <sohailaelbasel@gmail.com>
2021-09-13 06:38:26 +0200

    create the database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=11)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    category = models.ManyToManyField(Category, related_name='products')
+
+    def __str__(self):
+        return self.name
